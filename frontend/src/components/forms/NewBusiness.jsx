b15cce4e3c193ced0fe8eb4f3734b6c80b751136
Kevin Pasquarella <kevinpasquarella@Kevins-MBP-2.lan>
2024-04-30 08:28:44 -0400

    add create business table and use rowstack

--- frontend/src/components/forms/NewBusiness.jsx
+++ frontend/src/components/forms/NewBusiness.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import { useMutation } from 'react-query';
-import { FormControl, FormLabel, Input, Textarea, Button } from "@chakra-ui/react";
+import EditableTable from '../tables/EditableTable';
 
 const API_BASE_URL = 'http://localhost:8000/actions';
+const DATA_BASE_URL = 'http://localhost:8000/data';
 
 function NewBusiness() {
-  const [formData, setFormData] = useState({
-    name: '',
-    location: '',
-    description: '',
-    biz_type: ''
-  });
+  const [data, setData] = useState([]);
+
+  const columns = useMemo(() => [
+    {name: 'Name', id: 'name'},
+    {name: 'Location', id: 'location'},
+    {name: 'Business Type', id: 'biz_type'},
+    {name: 'Description', id: 'description'},
+  ]);
+
+  useEffect(() => {
+    fetchBusinesses();
+  }, []);
+
+  const fetchBusinesses = async () => {
+    const response = await fetch(`${DATA_BASE_URL}/get_businesses`);
+    const data = await response.json();
+    setData(data);
+  }
 
   const createBusiness = async (data) => {
     const response = await fetch(`${API_BASE_URL}/add_business`, {
@@ -27,48 +40,15 @@ function NewBusiness() {
   const { mutate: createBusinessMutation, isLoading: isCreatingBusiness } = useMutation(createBusiness, {
     onSuccess: () => {
       alert('Business added successfully');
-      setFormData({
-        name: '',
-        location: '',
-        description: '',
-        biz_type: ''
-      });
     }
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevData => ({
-      ...prevData,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createBusinessMutation(formData);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl id="name" isRequired>
-        <FormLabel>Name</FormLabel>
-        <Input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
-      </FormControl>
-      <FormControl id="location">
-        <FormLabel>Location</FormLabel>
-        <Input type="text" name="location" value={formData.location} onChange={handleChange} placeholder="Location" />
-      </FormControl>
-      <FormControl id="description">
-        <FormLabel>Description</FormLabel>
-        <Textarea name="description" value={formData.description} onChange={handleChange} placeholder="Description" />
-      </FormControl>
-      <FormControl id="biz_type">
-        <FormLabel>Business Type</FormLabel>
-        <Input type="text" name="biz_type" value={formData.biz_type} onChange={handleChange} placeholder="Business Type" />
-      </FormControl>
-      <Button type="submit" isLoading={isCreatingBusiness}>Add Business</Button>
-    </form>
+    <EditableTable 
+      columns={columns}
+      endpoint={createBusinessMutation}
+      data={data}
+      />
   );
 }
 
